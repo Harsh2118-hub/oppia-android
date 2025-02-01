1c14e4ffdd9e62689b8f2863407ca902c1effaf7
Harsh2118-hub <22bit125@ietdavv.edu.in>
2025-02-01 15:55:06 +0530

    update

--- scripts/src/java/org/oppia/android/scripts/xml/TextViewStyleCheck.kt
+++ scripts/src/java/org/oppia/android/scripts/xml/TextViewStyleCheck.kt
@@ -189,12 +189,6 @@ private class TextViewStyleCheck {
 
 // TODO(#5661): Add missing styles for TextView IDs.
 private val attributeIds = listOf(
-  "@+id/developer_options_text_view",
-  "@+id/onboarding_language_text_view",
-  "@+id/walkthrough_final_title_text_view",
-  "@+id/chapter_index",
-  "@+id/chapter_index",
-  "@+id/test_text_view",
   "@+id/feedback_text_view",
   "@+id/item_selection_contents_text_view",
   "@+id/learner_analytics_sync_status_text_view",
